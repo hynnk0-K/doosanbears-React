ebeeb9d1935e9594904e6b217c8509847c96d8b2
deliondane <hynnk0@gmail.com>
2025-01-20 09:58:45 +0900

    server 등록 config 변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,4 @@
 // export const API_URL=process.env.NODE_ENV==='production'? "https://port-0-petshop-server-m2ygh0fn97061ee8.sel4.cloudtype.app" : "http://localhost:8080"
 // export const API_URL=process.env.NODE_ENV==="http://localhost:5001"
-export const API_URL = "http://localhost:5001";
+// export const API_URL = "http://localhost:5001";
+export const API_URL=process.env.NODE_ENV==='production' ? "https://port-0-doosanbears-server-m5uc03btba946fd4.sel4.cloudtype.app" : "http://localhost:5001"
