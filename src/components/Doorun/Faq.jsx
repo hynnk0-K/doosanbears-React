dcc61d5220871d20d2c915bff377ef7cf17d3ad0
Bessy-kim <hynnk0@gmail.com>
2025-02-22 10:26:01 +0900

    constants sever 주소 추가

--- src/components/Doorun/Faq.jsx
+++ src/components/Doorun/Faq.jsx
@@ -1,22 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import faqData from "../../data/faqData.json";
 
 const Faq = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const queryParams = new URLSearchParams(location.search);
-  const initialPage = parseInt(queryParams.get('currentPage')) || 1;
+  const initialPage = parseInt(queryParams.get("currentPage")) || 1;
 
   const [currentPage, setCurrentPage] = useState(initialPage);
 
   const itemsPerPage = 9;
   const totalPages = Math.ceil(faqData.length / itemsPerPage);
-  const currentFaq = faqData.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+  const currentFaq = faqData.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  //로그인 검증
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    fetch("/auth/status", { credentials: "include" }) // `credentials` 옵션 수정
+      .then((res) => res.json())
+      .then((data) => setIsAuthenticated(data.isAuthenticated))
+      .catch((err) => console.error("인증 확인 중 오류 발생:", err));
+  }, []);
 
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
-    searchParams.set('currentPage', currentPage);
+    searchParams.set("currentPage", currentPage);
     navigate(`?${searchParams.toString()}`, { replace: true });
   }, [currentPage, location.search, navigate]);
 
@@ -28,7 +41,10 @@ const Faq = () => {
     const buttons = [];
     for (let i = 1; i <= totalPages; i++) {
       buttons.push(
-        <button key={i} type="button" className={currentPage === i ? "on" : ""} 
+        <button
+          key={i}
+          type="button"
+          className={currentPage === i ? "on" : ""}
           onClick={() => handlePageChange(i)}
         >
           {i}
@@ -44,28 +60,51 @@ const Faq = () => {
         <section>
           <h3 className="hide">FAQ</h3>
           <div className="list_top">
-            <p className="num">전체 <strong>{faqData.length}</strong></p>
+            <p className="num">
+              전체 <strong>{faqData.length}</strong>
+            </p>
           </div>
           <ul className="list_box">
-            {currentFaq.map(({id, title, user}) => (
+            {currentFaq.map(({ id, title, user }) => (
               <li key={id}>
-              <Link className="pop_open" to={`/doorundoorun/faq/${id}?currentPage=${currentPage}`}>
-                <div className="txt_area">
-                  <div className="tit">{title}</div>
-                  <div className="txt">{user}</div>
-                </div>
-              </Link>
-            </li>
+                <Link
+                  className="pop_open"
+                  to={`/doorundoorun/faq/${id}?currentPage=${currentPage}`}
+                >
+                  <div className="txt_area">
+                    <div className="tit">{title}</div>
+                    <div className="txt">{user}</div>
+                  </div>
+                </Link>
+              </li>
             ))}
           </ul>
           <div className="pagination">
-            <button className="first" title="처음" onClick={() => handlePageChange(1)} disabled={currentPage === 1}></button>
-            <button className="prev" title="이전" onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}></button>
-            <div className="pages">
-              {renderPaginationButtons()}
-            </div>
-            <button className="next" title="다음" onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages}></button>
-            <button className="last" title="마지막" onClick={() => handlePageChange(totalPages)} disabled={currentPage === totalPages}></button>
+            <button
+              className="first"
+              title="처음"
+              onClick={() => handlePageChange(1)}
+              disabled={currentPage === 1}
+            ></button>
+            <button
+              className="prev"
+              title="이전"
+              onClick={() => handlePageChange(currentPage - 1)}
+              disabled={currentPage === 1}
+            ></button>
+            <div className="pages">{renderPaginationButtons()}</div>
+            <button
+              className="next"
+              title="다음"
+              onClick={() => handlePageChange(currentPage + 1)}
+              disabled={currentPage === totalPages}
+            ></button>
+            <button
+              className="last"
+              title="마지막"
+              onClick={() => handlePageChange(totalPages)}
+              disabled={currentPage === totalPages}
+            ></button>
           </div>
         </section>
       </div>
